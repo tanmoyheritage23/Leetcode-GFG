bba180d71251de913f2eb2ed9b328755a76561dc
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-02-08 13:06:27 +0530

    Time: 6 ms (75.89%), Space: 9 MB (37.05%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,31 +3,31 @@ class Solution
     public:
         vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
         {
+            stack<int> st;
             unordered_map<int, int> mp;
-            for (int i = 0; i < nums2.size(); i++)
+            int n = nums2.size();
+            for (int i = n - 1; i >= 0; i--)
             {
-                mp[nums2[i]] = i;
+            	// itreting from n to 0 because we want next greater element
+                int curr = nums2[i];
+               	// pushing elements into the stack in decreasing order
+                while (!st.empty() && curr > st.top()) st.pop();
+               	// if anytime stack becomes empty that means there is no next greater element exists 
+               	// for my current element
+                if (st.empty()) mp[curr] = -1;
+               	// if not then then top element of the stack will be my next greater element for my 
+               	// current element because the stack is in monotonic decreasing order
+                else mp[curr] = st.top();
+               	// And most importantly before going for another iteration make sure you push your
+               	// current element into the stack
+                st.push(curr);
+            }
+            vector<int> ans;
+            for (int num: nums1)
+            {
+                ans.push_back(mp[num]);
             }
 
-            vector<int> ans;
-            for (int i = 0; i < nums1.size(); i++)
-            {
-                int curr = nums1[i];
-                int var = -1;
-                if (mp.count(curr))
-                {
-                    int ind = mp[curr];
-                    for (int j = ind + 1; j < nums2.size(); j++)
-                    {
-                        if (nums2[j] > curr)
-                        {
-                            var = nums2[j];
-                            break;
-                        }
-                    }
-                    ans.push_back(var);
-                }
-            }
             return ans;
         }
 };
