6be4f18899cebb3838cf1f04df8c6bde595f817a
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-03-25 21:11:57 +0530

    Time: 116 ms (68.03%), Space: 67.8 MB (17.90%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,11 +4,13 @@ public:
         int maxi = INT_MIN;
         int sum = 0;
         int n = nums.size();
+        int st, ansSt, ansEnd = 0;
         
         for(int i = 0; i < n; i++){
+            if(sum == 0) st = i;
             sum += nums[i];
             
-            if(sum > maxi) maxi = sum;
+            if(sum > maxi) maxi = sum, ansSt = st, ansEnd = i;
             
             if(sum < 0) sum = 0;
         }
