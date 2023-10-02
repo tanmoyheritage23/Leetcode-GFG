e40b1cc65cb2e58fa3a3c8c5d127ff03c7674f86
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-10-03 00:03:36 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (6.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,29 +3,24 @@ class Solution
     public:
         vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
         {
-            stack<int> st;
-            unordered_map<int, int> mp;
             int n = nums2.size();
+            unordered_map<int, int> mp;
+            stack<int> st;
+
             for (int i = n - 1; i >= 0; i--)
             {
-            	// itreting from n to 0 because we want next greater element
-                int curr = nums2[i];
-               	// pushing elements into the stack in decreasing order
-                while (!st.empty() && curr > st.top()) st.pop();
-               	// if anytime stack becomes empty that means there is no next greater element exists 
-               	// for my current element
-                if (st.empty()) mp[curr] = -1;
-               	// if not then then top element of the stack will be my next greater element for my 
-               	// current element because the stack is in monotonic decreasing order
-                else mp[curr] = st.top();
-               	// And most importantly before going for another iteration make sure you push your
-               	// current element into the stack
-                st.push(curr);
+                while (!st.empty() && nums2[st.top()] <= nums2[i]) st.pop();
+                if (!st.empty()) mp[nums2[i]] = st.top();
+                else mp[nums2[i]] = n;
+                st.push(i);
             }
+
             vector<int> ans;
-            for (int num: nums1)
+            for (int i = 0; i < nums1.size(); i++)
             {
-                ans.push_back(mp[num]);
+                int nextGreaterInd = mp[nums1[i]];
+                if (nextGreaterInd == n) ans.push_back(-1);
+                else ans.push_back(nums2[nextGreaterInd]);
             }
 
             return ans;
