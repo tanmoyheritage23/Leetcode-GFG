4281732b06f3c5d07eb3991cc2e045e151000e16
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-10-03 14:02:02 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (7.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,19 @@
 class Solution
 {
-    private:
-        int maximumMoney(int ind, vector<int> nums, vector<int>& dp)
-        {
-            if (ind == 0) return nums[ind];
-            if (ind < 0) return 0;
-            
-            if(dp[ind] != -1) return dp[ind];
-
-            int pick = nums[ind] + maximumMoney(ind - 2, nums,dp);
-            int not_pick = 0 + maximumMoney(ind - 1, nums,dp);
-
-            return dp[ind] = max(pick, not_pick);
-        }
     public:
         int rob(vector<int> &nums)
         {
             int n = nums.size();
-            vector<int>dp(n,-1);
-            return maximumMoney(n - 1, nums,dp);
+            vector<int> dp(n + 2, 0);
 
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int decidedToRob = nums[i] + dp[i + 2];
+                int notDecidedToRob = 0 + dp[i + 1];
+                int maxi = max(decidedToRob, notDecidedToRob);
+                dp[i] = maxi;
+            }
+
+            return dp[0];
         }
 };
