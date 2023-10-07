c2136721e7ef1939e6f98128737d4d207e3ad874
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-10-07 14:58:52 +0530

    Time: 2 ms (64.47%), Space: 10 MB (24.76%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -4,29 +4,20 @@ class Solution
     public:
         int mincostTickets(vector<int> &days, vector<int> &costs)
         {
-            int n = days.size();
-            vector<int> dp(days[n - 1] + costs[2], 0);
+            queue<pair<int, int>> week, month;	// day,cost
+            int ans = 0;
 
-            for (int ind = n-1; ind >= 0; ind--)
+            for (int day: days)
             {
-                int oneDayPassCost = costs[0];
-                int sevenDayPassCost = costs[1];
-                int thirtyDayPassCost = costs[2];
+                while (!week.empty() && week.front().first + 7 <= day) week.pop();
+                while (!month.empty() && month.front().first + 30 <= day) month.pop();
 
-                int indAfter1DayPass = lower_bound(days.begin(), days.end(), days[ind] + 1) - days.begin();
-                int take1DayPass = oneDayPassCost + dp[indAfter1DayPass];
+                week.push(make_pair(day, ans + costs[1]));
+                month.push(make_pair(day, ans + costs[2]));
 
-                int indAfter7DayPass = lower_bound(days.begin(), days.end(), days[ind] + 7) - days.begin();
-                int take7DayPass = sevenDayPassCost + dp[indAfter7DayPass];
-
-                int indAfter30DayPass = lower_bound(days.begin(), days.end(), days[ind] + 30) - days.begin();
-                int take30DayPass = thirtyDayPassCost + dp[indAfter30DayPass];
-
-                int minCost = min(take1DayPass, min(take7DayPass, take30DayPass));
-
-                dp[ind] = minCost;
+                ans = min(ans + costs[0], min(week.front().second, month.front().second));
             }
 
-            return dp[0];
+            return ans;
         }
 };
