e3615ab0fb25bfb9fda5ab0cfe26013aafb0d4bb
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-10-26 14:10:47 +0530

    Time: 162 ms (54.57%), Space: 105.9 MB (5.26%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,51 @@
+class Solution
+{
+    private:
+        bool canPlaceBalls(int m, int n, vector<int> position, int partition)
+        {
+            int ball = 1;
+            int lastPosition = position[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                int currPosition = position[i];
+                if (currPosition - lastPosition >= partition)
+                {
+                    ball++;
+                    lastPosition = currPosition;
+                    if (ball == m)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    public:
+        int maxDistance(vector<int> &position, int m)
+        {
+            int n = position.size();
+            sort(position.begin(), position.end());
+            int st = 0;
+            int end = position[n - 1] - position[0];
+
+            int minMagneticForce = 0;
+            while (st <= end)
+            {
+                int mid = st + (end - st) / 2;
+                bool canPlace = canPlaceBalls(m, n, position, mid);
+                if (canPlace)
+                {
+                    minMagneticForce = mid;
+                    st = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return minMagneticForce;
+        }
+};
