4d3982aefca76c97c27fbd4a971e131d453e5ef5
tanmoyheritage23 <89009776+tanmoyheritage23@users.noreply.github.com>
2023-12-16 01:05:22 +0530

    Time: 59 ms (60.88%), Space: 13.7 MB (55.97%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,53 +1,43 @@
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
-        int n = nums.size();
-        
-        vector<vector<int>>res;
-        
-        sort(nums.begin(),nums.end());
-        
-        for(int i = 0; i < n - 3; i++){
-            
-            if(i != 0 && nums[i] == nums[i - 1]) continue;
-            
-            for(int j = i + 1; j < n - 2; j++){
-                
-                if(j != i + 1 && nums[j] == nums[j - 1]) continue;
-               
-                int left = j + 1; int right = n - 1;
-                
-                while(left < right){
-                    
-                   long int sum = (long int) nums[i] + nums[j] + nums[left] + nums[right];
-                    
-                    if(sum < target) left++;
-                    
-                    else if(sum > target) right--;
-                    
-                    else {
-                        
-                        vector<int>ans;
-                        
-                        ans.push_back(nums[i]);
-                        ans.push_back(nums[j]);
-                        ans.push_back(nums[left]);
-                        ans.push_back(nums[right]);
-                        
-                        res.push_back(ans);
-                        
-                        left++; right--;
-                        
-                        while(left < right && nums[left] == nums[left - 1]) left++;
-                        while(left < right && nums[right] == nums[right + 1]) right--;
-                        
+class Solution
+{
+    public:
+        vector<vector < int>> fourSum(vector<int> &nums, int target)
+        {
+           	// -2 -1 0 0 1 2 
+            int n = nums.size();
+            sort(nums.begin(), nums.end());
+            vector<vector < int>> res;
+            for (int a = 0; a < n - 3; a++)
+
+            {
+                if (a != 0 && nums[a] == nums[a - 1]) continue;
+                for (int b = a + 1; b < n - 2; b++)
+
+                {
+                    if (b != a + 1 && nums[b] == nums[b - 1]) continue;
+                    int c = b + 1, d = n - 1;
+
+                    while (c < d)
+                    {
+                        long int sum = (long int) nums[a] + nums[b] + nums[c] + nums[d];
+                        if (sum == target)
+                        {
+                            vector<int> vec;
+                            vec.push_back(nums[a]);
+                            vec.push_back(nums[b]);
+                            vec.push_back(nums[c]);
+                            vec.push_back(nums[d]);
+                            res.push_back(vec);
+                            c++, d--;
+                            while (c < d && nums[c] == nums[c - 1]) c++;
+                            while (c < d && nums[d] == nums[d + 1]) d--;
+                        }
+                        else if (sum > target) d--;
+                        else c++;
                     }
                 }
             }
-            
+
+            return res;
         }
-        
-        return res;
-    }
 };
