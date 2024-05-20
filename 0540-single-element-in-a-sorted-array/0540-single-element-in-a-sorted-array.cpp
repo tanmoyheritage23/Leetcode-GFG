0e35244d362321cb08bfcd4c6eaca4405a51823f
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2024-05-20 13:47:35 +0530

    Time: 22 ms (31.46%), Space: 24.7 MB (13.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,8 +4,8 @@ class Solution
         int singleNonDuplicate(vector<int> &nums)
         {
             int low = 0;
-            int high = nums.size() - 2;
-            while (low <= high)
+            int high = nums.size() - 1;
+            while (low < high)
             {
                 int mid = (low + high) >> 1;
                 if (nums[mid] == nums[mid ^ 1])
@@ -14,7 +14,7 @@ class Solution
                 }
                 else
                 {
-                    high = mid - 1;
+                    high = mid ;
                 }
             }
 
