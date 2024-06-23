e532c8ccc5d4681d9f916aa100610b0d69166bea
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2024-06-24 00:33:57 +0530

    Time: 126 ms (77.78%), Space: 154.8 MB (100.00%) - LeetHub

--- 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
+++ 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int ans = 0, flipped = 0;
+        int ans = 0, flipped = 0; // flipped help us to know before encountering any index how many times the flipped operation has taken place (odd or even)
         
         for(int &num: nums){
-            if(num ^ flipped == 0){
+            if(num ^ flipped == 0){   // if nums[i] = 0 and flipped is even that means we need to flip that index to make it 1 and if nums[i] = 1 and flipped is odd that means we need to flip that index too because that 1 has become 0 after odd number of flip operation. At any other case we don't need to flip it.
                 
                 flipped ^= 1;
                 ans++;
