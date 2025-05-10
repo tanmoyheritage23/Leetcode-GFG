c558dc217573206009f134ee511057b01905e10e
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-05-10 18:04:45 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (21.7%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int goal = n - 1;
+
+        for(int i = n - 2; i >= 0; i--){
+            if(i + nums[i] >= goal){
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
