2a516d7a6028fb90d68842ff62207784d3f55464
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-05-27 20:42:57 +0530

    Time: 4 ms (97.15%), Space: 46.5 MB (9.12%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    // Approach:
+    // Using binary search and a sliding window, find the midpoint where,
+    // the integers between midpoint and midpoint + k is the k closest integers to x.
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        // The sliding window is between 'mid' and 'mid' + k.
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int midpoint = left + (right - left) / 2; // same as (left + right) / 2
+
+            // With midpoint on the left, we use x - arr[midpoint], while arr[midpoint + k] - x because it is on the right.
+            // This is important!
+            // Rather than using Math.abs(), we need the direction keep the x within the sliding window.
+            // If the window is too far left, we shift the window to the right.
+            if (x - arr[midpoint] > arr[midpoint + k] - x) {
+                left = midpoint + 1;
+            }
+            // If the window is too far right, we shift the window to the left.
+            else {
+                right = midpoint;
+            }
+        }
+
+        // Input all the k closest integers into the result.
+        List<Integer> result = new ArrayList<>(k);
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
