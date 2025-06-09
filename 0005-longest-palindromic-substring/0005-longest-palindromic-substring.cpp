38f969e6cab3ea62bc0f28068d873bb0dfaae9e7
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-06-09 14:20:21 +0530

    Time: 79 ms (49.33%), Space: 26.3 MB (31.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-    if(s.length()<=1){
-        return s;
-    }
-        
-        
-    int max_len=1;int n=s.length();
-    int st=0;int end=0;
-        
-    for(int i=0;i<n-1;i++){
-    int l=i; int h=i+1;
-     while(l>=0 && h<n){
-            if(s[l]==s[h]){
-                l--;
-                h++;
-            }else
-                break;
+    std::string longestPalindrome(std::string s) {
+        if (s.length() <= 1) {
+            return s;
         }
-        int len=h-l-1;
-        if(len>max_len){
-            max_len=len;
-            st=l+1;
-            end=h-1;
+        
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j < 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
         }
-}
 
- for(int i=0;i<n-1;i++){
-    int l=i; int h=i;
-     while(l>=0 && h<n){
-            if(s[l]==s[h]){
-                l--;
-                h++;
-            }else
-                break;
-        }
-        int len=h-l-1;
-        if(len>max_len){
-            max_len=len;
-            st=l+1;
-            end=h-1;
-        }
- }
-    
-    return s.substr(st,max_len);
-    
-}
+        
+        
+        return s.substr(start, end - start + 1);
+    }
 };
