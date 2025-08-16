bd031c81cf3bd09d898d66e916cf0ff47e06fe8f
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-08-16 15:30:32 +0530

    Time: 11 ms (33.33%), Space: 54.9 MB (5.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,28 @@
 class Solution {
     public int change(int amount, int[] coins) {
-       int n = coins.length;
-       int[][] dp = new int[n][amount+1];
-       for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-       return coinChange(coins,n-1,amount,dp); 
-    }
-    public int coinChange(int[] coins, int index, int amount, int[][] dp)
-    {
-        if(index == 0){
-            if(amount % coins[0] == 0) return 1;
-            else return 0;
-        }
-        if(dp[index][amount] != -1) return dp[index][amount];
-        int notTake = coinChange(coins, index - 1, amount,dp);
-        int take = 0;
-        if(amount >= coins[index]) take += coinChange(coins, index, amount-coins[index],dp); 
+        int N = coins.length;
+        long[][] dp = new long[N][amount + 1];
 
-        return dp[index][amount] = take + notTake;
-        
+        // Initializing base condition
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = 1;
+            /* Else condition is automatically fulfilled,
+            as dp array is initialized to zero */
+        }
+
+        for (int ind = 1; ind < N; ind++) {
+            for (int target = 0; target <= amount; target++) {
+                int notTaken = (int)dp[ind - 1][target];
+
+                int taken = 0;
+                if (coins[ind] <= target)
+                    taken = (int)dp[ind][target - coins[ind]];
+
+                dp[ind][target] = (notTaken + taken);
+            }
+        }
+        // Return the result
+        return (int)dp[N - 1][amount];
     }
 }
