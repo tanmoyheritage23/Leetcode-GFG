e16b41cad450271f475e63f0e458c82e809604c4
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-08-30 21:59:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long N = n;
+        if(n < 0) N = -1 * N; // for integer overflow
+
+        while(N > 0){
+            if(N % 2 == 1){
+                ans = ans * x;
+                N = N - 1;
+            }
+            else{
+                x = x*x;
+                N = N/2;
+            }
+        }
+
+        return n<0 ? 1/ans : ans;
+
+    }
+}
