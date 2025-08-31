885a454bfadbc55948a5affeac2959a974608eec
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-08-31 19:53:04 +0530

    Time: 0 ms (100%), Space: 42.4 MB (25.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        /* Transpose matrix */
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        /* reverse each row */
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+
+        
+    }
+}
