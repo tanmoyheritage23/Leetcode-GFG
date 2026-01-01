b37eb7d9d9f2a5ddc8b31ace1380bdb92e01964a
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2026-01-01 17:10:32 +0530

    Time: 19 ms (95.98%), Space: 76.6 MB (77.39%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -1,57 +1,52 @@
 class Solution {
 
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    int R, C;
+    int[] parent;
+
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) parent[pa] = pb;
+    }
 
     public int latestDayToCross(int row, int col, int[][] cells) {
-        R = row;
-        C = col;
 
-        boolean[][] water = new boolean[R][C];
+        int total = row * col;
+        int TOP = total;
+        int BOTTOM = total + 1;
 
-        for (int day = 0; day < cells.length; day++) {
+        parent = new int[total + 2];
+        for (int i = 0; i < total + 2; i++) parent[i] = i;
 
-            // Flood today's cell
-            int r = cells[day][0] - 1;
-            int c = cells[day][1] - 1;
-            water[r][c] = true;
+        boolean[][] land = new boolean[row][col];
 
-            boolean[][] visited = new boolean[R][C];
-            boolean canCross = false;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
-            // Try DFS from every top row land cell
-            for (int j = 0; j < C; j++) {
-                if (!water[0][j] && !visited[0][j]) {
-                    if (dfs(0, j, water, visited)) {
-                        canCross = true;
-                        break;
-                    }
+        for (int i = cells.length - 1; i >= 0; i--) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            land[r][c] = true;
+
+            int id = r * col + c;
+
+            if (r == 0) union(id, TOP);
+            if (r == row - 1) union(id, BOTTOM);
+
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col && land[nr][nc]) {
+                    union(id, nr * col + nc);
                 }
             }
 
-            // If crossing not possible → previous day was last valid
-            if (!canCross) return day;
+            if (find(TOP) == find(BOTTOM)) return i;
         }
 
         return 0;
-    }
-
-    private boolean dfs(int r, int c, boolean[][] water, boolean[][] visited) {
-        if (r == R - 1) return true; // reached bottom
-
-        visited[r][c] = true;
-
-        for (int[] d : dirs) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-
-            if (nr >= 0 && nr < R && nc >= 0 && nc < C &&
-                !water[nr][nc] && !visited[nr][nc]) {
-
-                if (dfs(nr, nc, water, visited))
-                    return true;
-            }
-        }
-        return false;
     }
 }
