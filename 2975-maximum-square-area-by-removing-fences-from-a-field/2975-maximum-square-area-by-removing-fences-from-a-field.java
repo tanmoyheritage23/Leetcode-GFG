553bc23287b6007df8ee79fa8fed439c7f45e483
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2026-01-17 10:01:43 +0530

    Time: 855 ms (10%), Space: 272.9 MB (42%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.java
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.java
@@ -28,11 +28,10 @@ class Solution {
 
         list.add(1);
         list.add(border);
-        Collections.sort(list);
 
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
-                set.add(list.get(j) - list.get(i));
+                set.add(Math.abs(list.get(j) - list.get(i)));
             }
         }
 
