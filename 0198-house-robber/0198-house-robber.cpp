58c13ab9bb0bf95eca3d29ded49a46b99cefab2b
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2026-03-07 16:25:34 +0530

    Time: 0 ms (100%), Space: 7.6 MB (100%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,22 @@
-class Solution
-{
-    public:
-        int rob(vector<int> &nums)
-        {
-            int n = nums.size();
-            vector<int> dp(n + 2, 0);
-
-            for (int i = n - 1; i >= 0; i--)
-            {
-                int decidedToRob = nums[i] + dp[i + 2];
-                int notDecidedToRob = 0 + dp[i + 1];
-                int maxi = max(decidedToRob, notDecidedToRob);
-                dp[i] = maxi;
-            }
-
-            return dp[0];
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        
+        vector<int>dp(n,0);
+        
+        dp[0]=nums[0];
+        
+        for(int i=1;i<n;i++){
+            
+            int pick=nums[i];
+            
+            if(i>1) pick+=dp[i-2];
+            
+            int notpick=0+dp[i-1];
+            
+            dp[i]=max(pick,notpick);
         }
+        return dp[n-1];
+    }
 };
