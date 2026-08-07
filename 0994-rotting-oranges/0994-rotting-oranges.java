6d729762daec772f752d65df73b0218f13208f02
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2026-08-07 12:38:07 +0530

    Time: 4 ms (4.58%), Space: 44 MB (85.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,54 +2,45 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        int[][] vis = new int[n][m];
-        Queue<int[]> q = new LinkedList<>();
-
-        // Add all initially rotten oranges to queue
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j, 0}); // row, col, time
-                    vis[i][j] = 2;
-                } else {
-                    vis[i][j] = 0;
+        int count = 0;
+        Queue<int[]>q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i,j});
+                }else if(grid[i][j] == 1){
+                    count++;
                 }
             }
         }
+        if(count == 0) return 0;
 
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-        int tm = 0;
+        int[] dx = {1,0,-1,0};
+        int[] dy = {0,1,0,-1};
 
-        while (!q.isEmpty()) {
-            int[] front = q.poll();
-            int r = front[0];
-            int c = front[1];
-            int t = front[2];
-            tm = Math.max(tm, t);
+        int time = 0;
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-- > 0){
+                int[] pos = q.poll();
+                int row = pos[0], col = pos[1];
+                for(int i = 0; i < 4; i++){
+                    int nrow = row + dx[i];
+                    int ncol = col + dy[i];
 
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-                        && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-                    q.offer(new int[]{nrow, ncol, t + 1});
-                    vis[nrow][ncol] = 2;
+                    if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1){
+                        grid[nrow][ncol] = 2;
+                        count--;
+                        q.offer(new int[]{nrow,ncol});
+                    }
                 }
             }
+            time++;
         }
 
-        // Check if any fresh orange is left
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && vis[i][j] != 2) {
-                    return -1;
-                }
-            }
-        }
+        return count == 0 ? time-1 : -1;
 
-        return tm;
+
     }
 }
